604d9ed884fcbd2482ae88174529f3d40cbd5aa2
herman <fqingyang365@gmail.com>
2023-12-18 18:04:04 +0800

    feat:deploy file

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export' // SSG看到构建生成的HTML文件
 }
 
 module.exports = nextConfig
--- pages/ssg-1.js
+++
@@ -1,7 +0,0 @@
-// 当不获取数据，next默认是用的就是SSG
-
-function About() {
-  return <div>About</div>
-}
-
-export default About
